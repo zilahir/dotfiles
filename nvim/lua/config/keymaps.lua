743aef654fdcdce9f1d901b0457869fe56477a7d
Richard Zilahi <zilahi@gmail.com>
2024-01-09 12:10:37 +0200

    disabled arrow keys

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,3 +5,11 @@ map("n", "<leader>í", function() require("trouble").toggle() end)
 map("n", "<leader>íy", function() require("trouble").toggle("quickfix") end)
 map('n', '<leader>d', vim.diagnostic.open_float)
 map('n', '<leader>gb', function() require("gitsigns").blame_line{full=true} end)
+
+-- disabling arrow keys
+
+local opts = { noremap = true, silent = true }
+map('n', '<Up>', '<Nop>', opts)
+map('n', '<Down>', '<Nop>', opts)
+map('n', '<Left>', '<Nop>', opts)
+map('n', '<Right>', '<Nop>', opts)
