a31e09313803628a74a1bfcf64a2914438d76b00
Richard Zilahi <zilahi@gmail.com>
2024-01-09 12:36:02 +0200

    disabled arrow keys in every mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,7 +9,21 @@ map('n', '<leader>gb', function() require("gitsigns").blame_line{full=true} end)
 -- disabling arrow keys
 
 local opts = { noremap = true, silent = true }
+
+-- normal mode
 map('n', '<Up>', '<Nop>', opts)
 map('n', '<Down>', '<Nop>', opts)
 map('n', '<Left>', '<Nop>', opts)
 map('n', '<Right>', '<Nop>', opts)
+
+-- insert mode
+map('i', '<Up>', '<Nop>', opts)
+map('i', '<Down>', '<Nop>', opts)
+map('i', '<Left>', '<Nop>', opts)
+map('i', '<Right>', '<Nop>', opts)
+
+-- visual block mode
+map('v', '<Up>', '<Nop>', opts)
+map('v', '<Down>', '<Nop>', opts)
+map('v', '<Left>', '<Nop>', opts)
+map('v', '<Right>', '<Nop>', opts)
