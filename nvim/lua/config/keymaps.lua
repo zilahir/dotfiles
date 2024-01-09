dfe39f3f763194436646acd1c280c0e440783d4b
Richard Zilahi <zilahi@gmail.com>
2024-01-09 22:55:16 +0200

    re-eanbled arrow in insert mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -17,10 +17,10 @@ map('n', '<Left>', '<Nop>', opts)
 map('n', '<Right>', '<Nop>', opts)
 
 -- insert mode
-map('i', '<Up>', '<Nop>', opts)
-map('i', '<Down>', '<Nop>', opts)
-map('i', '<Left>', '<Nop>', opts)
-map('i', '<Right>', '<Nop>', opts)
+-- map('i', '<Up>', '<Nop>', opts)
+-- map('i', '<Down>', '<Nop>', opts)
+-- map('i', '<Left>', '<Nop>', opts)
+-- map('i', '<Right>', '<Nop>', opts)
 
 -- visual block mode
 map('v', '<Up>', '<Nop>', opts)
