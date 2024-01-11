92cfb099afdf640ff23d7c9b331b1d7eb71742b2
Richard Zilahi <zilahi@gmail.com>
2024-01-11 22:33:20 +0200

    harpoon init + cleanup

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -25,21 +25,15 @@ return {
             },
           },
           servers = {
-            -- tsserver will be automatically installed with mason and loaded with lspconfig
             tsserver = {},
             tailwindcss = {},
           },
-          -- you can do any additional lsp server setup here
-          -- return true if you don't want this server to be setup with lspconfig
           ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
           setup = {
-            -- example to setup with typescript.nvim
             tsserver = function(_, opts)
               require("typescript").setup({ server = opts })
               return true
             end,
-            -- Specify * to use this function as a fallback for any server
-            -- ["*"] = function(server, opts) end,
           },
         },
       },
