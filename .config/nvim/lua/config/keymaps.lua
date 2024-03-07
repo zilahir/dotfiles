4e19d31e0d10007945d5a5093045df855fb96717
Richard Zilahi <zilahi@gmail.com>
2024-03-07 18:28:55 +0200

    lazy & keymaps update

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,20 +5,26 @@ local mouse = vim.opt.mouse
 mouse = ""
 
 map("t", "<C-t>", "<cmd>close<cr>", { desc = "Hide Terminal" })
-map("n", "<leader>í", function() require("trouble").toggle() end)
-map("n", "<leader>íy", function() require("trouble").toggle("quickfix") end)
-map('n', '<leader>d', vim.diagnostic.open_float)
-map('n', '<leader>gb', function() require("gitsigns").blame_line{full=true} end)
+map("n", "<leader>í", function()
+  require("trouble").toggle()
+end)
+map("n", "<leader>íy", function()
+  require("trouble").toggle("quickfix")
+end)
+map("n", "<leader>d", vim.diagnostic.open_float)
+map("n", "<leader>gb", function()
+  require("gitsigns").blame_line({ full = true })
+end)
 
 -- disabling arrow keys
 
 local opts = { noremap = true, silent = true }
 
 -- normal mode
-map('n', '<Up>', '<Nop>', opts)
-map('n', '<Down>', '<Nop>', opts)
-map('n', '<Left>', '<Nop>', opts)
-map('n', '<Right>', '<Nop>', opts)
+map("n", "<Up>", "<Nop>", opts)
+map("n", "<Down>", "<Nop>", opts)
+map("n", "<Left>", "<Nop>", opts)
+map("n", "<Right>", "<Nop>", opts)
 
 -- insert mode
 -- map('i', '<Up>', '<Nop>', opts)
@@ -27,11 +33,17 @@ map('n', '<Right>', '<Nop>', opts)
 -- map('i', '<Right>', '<Nop>', opts)
 
 -- visual block mode
-map('v', '<Up>', '<Nop>', opts)
-map('v', '<Down>', '<Nop>', opts)
-map('v', '<Left>', '<Nop>', opts)
-map('v', '<Right>', '<Nop>', opts)
+map("v", "<Up>", "<Nop>", opts)
+map("v", "<Down>", "<Nop>", opts)
+map("v", "<Left>", "<Nop>", opts)
+map("v", "<Right>", "<Nop>", opts)
 
 -- quick vertical navigation
-map('n', '<C-j>', '10j', { noremap = true, silent = true })
-map('n', '<C-k>', '10k', { noremap = true, silent = true })
+map("n", "<C-j>", "10j", { noremap = true, silent = true })
+map("n", "<C-k>", "10k", { noremap = true, silent = true })
+
+-- copilot setup
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
