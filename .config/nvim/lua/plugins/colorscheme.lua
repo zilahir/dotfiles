22d290a1be1283a79717f1488eaef214f1d7f360
Richard Zilahi <zilahi@gmail.com>
2024-06-10 21:43:32 +0300

    colorscheme config update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,25 +1,26 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = true,
-        opts = {
-            style = "night",
-            on_highlights = function(hl, c)
-                hl.MiniIndentscopeSymbol = {
-                    fg = "#3b4261"
-                }
-                hl.IblIndent = {
-                    fg = "#242635"
-                }
-                hl["@constructor.tsx"] = { link = "@tag.tsx" }
-                hl["@tag.attribute.tsx"] = { link = "@parameter" }
-            end,
-        },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      on_highlights = function(hl, c)
+        hl.MiniIndentscopeSymbol = {
+          fg = "#3b4261",
+        }
+        hl.IblIndent = {
+          fg = "#242635",
+        }
+        hl["@constructor.tsx"] = { link = "@tag.tsx" }
+        hl["@tag.attribute.tsx"] = { link = "@parameter" }
+      end,
     },
-    {
-        "LazyVim/LazyVim",
-        opts = {
-          colorscheme = "tokyonight",
-        },
-    }
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
 }
