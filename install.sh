2dcc6df150e7303a54a2a02272b9bfe239322d70
Richard Zilahi <zilahi@gmail.com>
2024-08-26 00:14:22 +0300

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-# check if ~/tmpdotfiles exists
+# check if the target folder if exists, and its not empty
 
+if [-d ~/zilahir/dotfiles] && [-n "$(ls -A ~/zilahir/dotfiles)"]; then
+  echo "Directory ~/zilahir/dotfiles exists and is not empty, deleting"
+  rm -rf ~/zilahir/dotfiles
+  mkdir -p ~/zilahir/dotfiles
+fi
+
+# check if ~/tmpdotfiles exists
 if [ -d ~/tmpdotfiles ]; then
   echo "Directory ~/tmpdotfiles exists."
 else
