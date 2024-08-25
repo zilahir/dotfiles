9a457e08e2bfbdd7f508a6cb4723cdc832d82a49
Richard Zilahi <zilahi@gmail.com>
2024-08-26 00:03:58 +0300

    update install script

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+# check if ~/tmpdotfiles exists
+
+if [ -d ~/tmpdotfiles ]; then
+  echo "Directory ~/tmpdotfiles exists."
+else
+  mkdir -p ~/tmpdotfiles
+fi
+
 git clone --separate-git-dir=$HOME/zilahir/dotfiles https://github.com/zilahir/dotfiles.git ~/tmpdotfiles
 rsync --recursive --verbose --exclude '.git' ~/tmpdotfiles/ $HOME/
 rm -r ~/tmpdotfiles
